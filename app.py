6aa9bd2c194f9695c78e98212fcb947e97982949
Diegospf12 <diego.pacheco@utec.edu.pe>
2023-08-17 21:28:47 -0500

    tarea2

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask
-
 from flask import request
-
- 
 
 app = Flask(__name__)
 
- 
-
 @app.route('/')
-
 def hello():
     print("Mi modificación1")
- 
-
     return '<h1>Mi modificacion</h1>'
 
-def multiplicar(var1, var2):
-    print(var1 * var2)
+@app.route('/sumar')
+def sumar():
+    param1 = float(request.args.get('param1'))
+    param2 = float(request.args.get('param2'))
+    resultado = param1 + param2
+    return f'La suma de {param1} y {param2} es: {resultado}'
+
+@app.route('/multiplicar')
+def multiplicar():
+    param1 = float(request.args.get('param1'))
+    param2 = float(request.args.get('param2'))
+    resultado = param1 * param2
+    return f'El producto de {param1} y {param2} es: {resultado}'
+
+if __name__ == '__main__':
+    app.run()
+
